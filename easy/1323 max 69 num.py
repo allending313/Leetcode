0c8b92a36ea0af4633aa44fc4ab3a5aed4d022b2
Allen Ding <allending313@gmail.com>
2022-11-07 19:14:39 -0600

    11/7/22

---
+++ easy/1323 max 69 num.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        """my naive solution"""
+        # return int(str(num).replace('6', '9', 1))
+        
+        """mod"""
+        i = 0 
+        tem = num
+        sixidx = -1 
+        while tem > 0:
+            if tem % 10 == 6:
+                sixidx = i  #refresh sixidx when found 6 at large digit.
+            tem = tem//10
+            i += 1
+        return (num + 3 *(10**sixidx)) if sixidx != -1 else num
