9e43bb8e8c1507748f7a2c4504ed07ce431db0d2
Allen Ding <allending313@gmail.com>
2024-01-22 23:41:19 -0500

    24.1.22

---
+++ top150/add_binary.go
@@ -0,0 +1,30 @@
+func addBinary(a string, b string) string {
+	var result string
+
+	ai := len(a) - 1
+	bi := len(b) - 1
+	carry := 0
+
+	for ai >= 0 || bi >= 0 {
+        x, y := 0, 0
+		if ai >= 0 {
+			x = int(a[ai] - '0')
+			ai--
+		}
+		if bi >= 0 {
+			y = int(b[bi] - '0')
+			bi--
+		}
+		
+        sum := strconv.Itoa(carry ^ x ^ y)
+		carry = (carry & x) | (x & y) | (carry & y)
+
+        result = sum + result
+	}
+	
+	if carry == 1 {
+		result = "1" + result
+	}
+
+	return result
+}
