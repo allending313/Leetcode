8d53b81d3c6fc47b7e2f086ee8538710910d72cb
Allen Ding <allending313@gmail.com>
2024-01-23 00:01:36 -0500

    24.1.22

--- top150/sqrt.go
+++ top150/sqrt.go
@@ -1,15 +1,25 @@
+// func mySqrt(x int) int {
+//     l, r := 0, x + 1
+
+//     for l < r {
+//         m := l + (r - l) / 2
+
+//         if m * m > x {
+//             r = m
+//         } else {
+//             l = m + 1
+//         }
+//     }
+
+//     return l - 1
+// }
+
 func mySqrt(x int) int {
-    l, r := 0, x + 1
+	r := x
 
-    for l < r {
-        m := l + (r - l) / 2
-
-        if m * m > x {
-            r = m
-        } else {
-            l = m + 1
-        }
-    }
-
-    return l - 1
+	for r*r > x {
+		r = (r + x/r) / 2
+	}
+    
+	return r
 }
