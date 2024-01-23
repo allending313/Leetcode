e511a6290e4505bcf9f0b7ce0144a24667621862
Allen Ding <allending313@gmail.com>
2024-01-23 00:19:51 -0500

    24.1.23

---
+++ top150/remove_dup_sorted_linked.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func deleteDuplicates(head *ListNode) *ListNode {
+    curr := head
+    
+    for curr != nil {
+        if curr.Next != nil && curr.Val == curr.Next.Val {
+            curr.Next = curr.Next.Next
+            continue
+        }
+        curr = curr.Next
+    }
+    
+    return head
+}
