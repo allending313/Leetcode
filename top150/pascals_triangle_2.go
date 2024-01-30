a1a60bd461ff0ff9f34310663b5184c2abbe1e0f
Allen Ding <allending313@gmail.com>
2024-01-30 02:02:43 -0500

    24.1.30

---
+++ top150/pascals_triangle_2.go
@@ -0,0 +1,13 @@
+func getRow(rowIndex int) []int {
+    res := make([]int, rowIndex + 1)
+    res[0] = 1
+    
+    for i := 0; i <= rowIndex; i++ {
+        res[i] = 1
+        for j := i - 1 ; j > 0; j-- {
+            res[j] = res[j] + res[j-1]
+        }
+    }
+
+    return res
+}
