c7ce5ae95056f4cea4127d5753f12e09e009ca9f
Allen Ding <allending313@gmail.com>
2024-02-09 00:57:09 -0500

    24.2.9

---
+++ top150/is_subsequence.go
@@ -0,0 +1,9 @@
+func isSubsequence(s string, t string) bool {
+    i := 0
+    for j := 0; j < len(t) && i < len(s); j++ {
+        if t[j] == s[i] {
+            i++
+        }
+    }
+    return i == len(s)
+}
