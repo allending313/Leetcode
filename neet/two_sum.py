1f656d65d65048b4fd46c8eea9b3f3e610b7a771
Allen Ding <allending313@gmail.com>
2024-07-30 18:47:06 -0400

    24.07.30

---
+++ neet/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for i, x in enumerate(nums):
+            if target - x in m:
+                return m[target - x], i
+            m[x] = i
+        return
+            
