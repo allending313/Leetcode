72a1595b5ad38289def116910e4f334468355504
Allen Ding <allending313@gmail.com>
2024-10-08 14:08:21 -0400

    24.10.08

---
+++ neet/reverse_linked_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # prev = None
+        # while head:
+        #     temp = head.next
+        #     head.next = prev
+        #     prev = head
+        #     head = temp
+        
+        # return prev
+
+        if not (head and head.next):
+            return head
+        
+        temp = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return temp
+
+            
