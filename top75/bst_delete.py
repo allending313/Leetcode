6accfa4d063acf4302e890edac768a1b232eeaa0
Allen Ding <allending313@gmail.com>
2025-02-19 16:05:00 -0500

    25.02.19

---
+++ top75/bst_delete.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        def getSuc(curr):
+            curr = curr.right
+            while curr.left:
+                curr = curr.left
+            return curr
+        
+        if not root:
+            return None
+        
+        if root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            
+            suc = getSuc(root)
+            root.val = suc.val
+            root.right = self.deleteNode(root.right, suc.val)
+        
+        return root
